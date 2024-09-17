585aaf696dcfa864bdf1c7c27dc714dbb38888bd
huniens <huniens06@gmail.com>
2024-09-17 10:02:01 +0700

    modification 1

--- JS4_PHP/operator.php
+++ JS4_PHP/operator.php
@@ -15,5 +15,17 @@ echo "Hasil kali: {$hasilKali} <br>";
 echo "Sisa bagi: {$sisaBagi} <br>";
 echo "Pangkat: {$pangkat} <br>";
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Hasil sama: {$hasilSama} <br>";
+echo "Hasil kurang: {$hasilTidakSama} <br>";
+echo "Hasil kali: {$hasilLebihKecil} <br>";
+echo "Sisa bagi: {$hasilLebihBesar} <br>";
+echo "Lebih kecil sama: {$hasilLebihKecilSama} <br>";
+echo "Lebih besar sama: {$hasilLebihBesarSama} <br>";
 ?>
-//first commit
