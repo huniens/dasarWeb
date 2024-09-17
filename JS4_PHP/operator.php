dac0e8fd407740d4c2bc427b20c2a26fb8d8f7f6
huniens <huniens06@gmail.com>
2024-09-17 09:47:49 +0700

    JS 4 STEP 1

---
+++ JS4_PHP/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil tambah: {$hasilTambah} <br>";
+echo "Hasil kurang: {$hasilKurang} <br>";
+echo "Hasil kali: {$hasilKali} <br>";
+echo "Sisa bagi: {$sisaBagi} <br>";
+echo "Pangkat: {$pangkat} <br>";
+
+?>
