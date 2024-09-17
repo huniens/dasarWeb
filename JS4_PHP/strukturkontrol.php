6db074cedf7aa1a2c56e8bcf198631abf7ad5adf
huniens <huniens06@gmail.com>
2024-09-17 10:37:40 +0700

    STEP 9

--- JS4_PHP/strukturkontrol.php
+++ JS4_PHP/strukturkontrol.php
@@ -24,4 +24,17 @@ $hari++;
 
 echo"<br>";
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+$jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+
+}
+echo"<br>";
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>  
