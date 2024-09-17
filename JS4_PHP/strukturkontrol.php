0043889ddbaf654aee4b09007e0074b2b1d14420
huniens <huniens06@gmail.com>
2024-09-17 10:41:00 +0700

    STEP 14

--- JS4_PHP/strukturkontrol.php
+++ JS4_PHP/strukturkontrol.php
@@ -37,4 +37,13 @@ $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
 echo"<br>";
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+echo"<br>";
+echo "Total skor ujian adalah: $totalSkor";
+
 ?>  
