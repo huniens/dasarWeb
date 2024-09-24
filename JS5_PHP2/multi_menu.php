acbefe3dc855cddaf914f3f52733d1aa2278a26c
huniens <huniens06@gmail.com>
2024-09-24 19:37:54 +0700

    JOBSHEET 5 - PRACTICAL 6 - QUESTION 12

--- JS5_PHP2/multi_menu.php
+++ JS5_PHP2/multi_menu.php
@@ -1,44 +1,34 @@
 <?php
 $menu = [
-    [
-        "nama" => "Beranda"
+    ["nama" => "Beranda"],
+    ["nama" => "Berita",
+     "subMenu" => [
+         ["nama" => "Wisata",
+          "subMenu" => [
+              ["nama" => "Pantai"],
+              ["nama" => "Gunung"]
+          ]
+         ],
+         ["nama" => "Kuliner"],
+         ["nama" => "Hiburan"]
+     ]
     ],
-    [
-        "nama" => "Berita",
-        "subMenu" => [
-            [
-                "nama" => "Wisata",
-                "subMenu" => [
-                    [
-                        "nama" => "Pantai"
-                    ],
-                    [
-                        "nama" => "Gunung"
-                    ]
-                ]
-            ],
-            [
-                "nama" => "Kuliner"
-            ],
-            [
-                "nama" => "Hiburan"
-            ],
-            [
-                "nama" => "Tentang"
-            ],
-            [
-                "nama" => "Kontak"
-            ]
-        ]
-    ]
+    ["nama" => "Tentang"],
+    ["nama" => "Kontak"]
 ];
 
-function tampilkanMenuBertingkat (array $menu) {
+function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
+    foreach ($menu as $item) {
         echo "<li>{$item['nama']}</li>";
+        
+        // Check if the current item has a sub-menu
+        if (isset($item['subMenu']) && is_array($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']); // Recursive call for sub-menu
+        }
     }
     echo "</ul>";
 }
+
 tampilkanMenuBertingkat($menu);
 ?>
