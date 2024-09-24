26665e9555c74d4ae15824f62f76f77964814efc
huniens <huniens06@gmail.com>
2024-09-24 09:45:35 +0700

    JOBSHEET 5 - PRACTICAL 4 - QUESTION 5

--- JS5_PHP2/function.php
+++ JS5_PHP2/function.php
@@ -1,12 +1,19 @@
 <?php
-    function perkenalan(){
-        echo "Assalamualaikum, ";
-        echo "Perkenalkan, nama saya Hanin<br/>"; //Tulis sesuai nama kalian
+    function perkenalan($nama, $salam){
+        echo $salam.", ";
+        echo "Perkenalkan, nama saya ".$nama."<br/>"; 
         echo "Senang berkenalan dengan Anda<br/>";
     }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+    //memanggil fungsi yang sudah dibuat
+    perkenalan("Hamdana","Hallo");
+
+    echo "<hr>";
+
+    $saya = "Elok";
+    $ucapanSalam = "Selamat pagi"; 
+
+    //memanggil fungsi yang sudah dibuat
+    perkenalan($saya, $ucapanSalam);
 
 ?>
