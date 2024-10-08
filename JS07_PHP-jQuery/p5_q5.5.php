1842f4a75ab52c133cdb9ae5c6c0a038ba170702
huniens <huniens06@gmail.com>
2024-10-08 19:29:17 +0700

    JOBSHEET 7 - PRACTICAL 5 - QUESTION 5.5

---
+++ JS07_PHP-jQuery/p5_q5.5.php
@@ -0,0 +1,10 @@
+<?php
+$pattern = '/go?d/'; // Matches "god" or "gd"
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+?>
