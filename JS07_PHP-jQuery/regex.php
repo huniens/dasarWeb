78299668b65f14ba522fdb2ea251381ed02795ee
huniens <huniens06@gmail.com>
2024-10-08 09:56:32 +0700

    JOBSHEET 7 - PRACTICAL 5 - QUESTION 5.4

--- JS07_PHP-jQuery/regex.php
+++ JS07_PHP-jQuery/regex.php
@@ -22,4 +22,12 @@ $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output "I like banana pie."
+
+$pattern = '/go*d/'; //cocokkan "god", "good", "gooood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan". $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
