1c172cc1b628a9277a644558092593637698661a
huniens <huniens06@gmail.com>
2024-10-08 09:27:45 +0700

    JOBSHEET 7 - PRACTICAL 5 - QUESTION 5.3

--- JS07_PHP-jQuery/regex.php
+++ JS07_PHP-jQuery/regex.php
@@ -17,4 +17,9 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
 }
 
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; // Output "I like banana pie."
 ?>
