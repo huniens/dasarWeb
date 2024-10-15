d37cf1926d2138da056cbf0ea505d37c6322bd22
huniens <huniens06@gmail.com>
2024-10-15 10:24:57 +0700

    JOBSHEET 8 - PRACTICAL 2 - QUESTION 9

--- JS08_PHP-FormUpload/proses_upload.php
+++ JS08_PHP-FormUpload/proses_upload.php
@@ -1,28 +1,62 @@
 <?php
-// Lokasi penyimpanan file yang diunggah
-$targetDirectory = "documents/";
+if (isset($_POST["submit"])) {
+    $targetdir = "uploads/"; // Direktori untuk menyimpan file yang diunggah
 
-// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
-if (!file_exists($targetDirectory)) {
-    mkdir($targetDirectory, 0777, true);
-}
+    // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+    if (!file_exists($targetdir)) {
+        mkdir($targetdir, 0777, true);
+    }
 
-if ($_FILES['files']['name'][0]) {
-    $totalFiles = count($_FILES['files']['name']);
+    $totalFiles = count($_FILES["myfile"]["name"]);
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5 * 1024 * 1024;
 
-    // Loop melalui semua file yang diunggah
+    // Loop untuk menangani beberapa file
     for ($i = 0; $i < $totalFiles; $i++) {
-        $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $fileName;
+        $fileName = basename($_FILES["myfile"]["name"][$i]);
+        $targetfile = $targetdir . $fileName;
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"][$i] <= $maxsize) {
+            if (move_uploaded_file($_FILES["myfile"]["tmp_name"][$i], $targetfile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+
+                // Generate thumbnail image
+                $thumbnailWidth = 200;
+                $thumbnailHeight = 0; // Otomatis hitung tinggi
+                $thumbnailFile = $targetdir . "thumb_" . $fileName;
+
+                $sourceImage = imagecreatefromstring(file_get_contents($targetfile));
+                $sourceWidth = imagesx($sourceImage);
+                $sourceHeight = imagesy($sourceImage);
+
+                if ($sourceWidth > $thumbnailWidth) {
+                    $thumbnailHeight = round(($thumbnailWidth / $sourceWidth) * $sourceHeight);
+                } else {
+                    $thumbnailWidth = $sourceWidth;
+                    $thumbnailHeight = $sourceHeight;
+                }
+
+                // Buat gambar thumbnail
+                $thumbnailImage = imagecreatetruecolor((int)$thumbnailWidth, (int)$thumbnailHeight);
+                imagecopyresampled($thumbnailImage, $sourceImage, 0, 0, 0, 0, (int)$thumbnailWidth, (int)$thumbnailHeight, $sourceWidth, $sourceHeight);
+
+                // Simpan gambar thumbnail
+                if (imagejpeg($thumbnailImage, $thumbnailFile, 90)) {
+                    echo "Thumbnail $fileName berhasil dibuat.<br>";
+                    echo "<img src='$thumbnailFile' width='$thumbnailWidth' height='$thumbnailHeight' alt='Thumbnail Image'><br>";
+                } else {
+                    echo "Gagal membuat thumbnail $fileName.<br>";
+                }
+
+                imagedestroy($sourceImage);
+                imagedestroy($thumbnailImage);
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
         } else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "File $fileName tidak valid atau melebihi ukuran maksimum.<br>";
         }
     }
-} else {
-    echo "Tidak ada file yang diunggah.";
 }
 ?>
