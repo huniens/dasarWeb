ac6305d3d2be4f350bc38e597f0d211fb8990f61
huniens <huniens06@gmail.com>
2024-10-15 20:31:14 +0700

    JOBSHEET 8 - PRACTICAL 3 - QUESTION 11

--- JS08_PHP-FormUpload/form_upload_ajax.php
+++ JS08_PHP-FormUpload/form_upload_ajax.php
@@ -2,17 +2,41 @@
 <html>
 
 <head>
-    <title>Unggah File Dokumen</title>
+    <title>Unggah File Gambar</title>
 </head>
 
 <body>
     <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="file" id="file">
-    <input type="submit" name="submit" value="Unggah">
+        <input type="file" name="files[]" id="file" multiple> <!-- Multiple files allowed -->
+        <input type="submit" name="submit" value="Unggah Gambar">
     </form>
     <div id="status"></div>
+
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="upload.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#upload-form").submit(function(e) {
+                e.preventDefault();
+
+                var formData = new FormData(this);
+
+                $.ajax({
+                    type: 'POST',
+                    url: 'upload_ajax.php',
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    success: function(response) {
+                        $("#status").html(response);
+                    },
+                    error: function() {
+                        $('#status').html('Terjadi kesalahan saat mengunggah file.');
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 
 </html>
