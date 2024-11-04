2c4ccffa2dc31dce2a62f8ba71550dc6d57d68fc
huniens <huniens06@gmail.com>
2024-11-04 23:34:37 +0700

    JOBSHEET 10 - PRACTICAL 6.2

--- JS10_CRUD/ajax/index.php
+++ JS10_CRUD/ajax/index.php
@@ -30,22 +30,26 @@ include 'auth.php';
                     <label>Nama</label>
                     <input type="hidden" name="id" id="id">
                     <input type="text" name="nama" id="nama" class="form-control" required>
+                    <p class="text-danger" id="err_nama"></p>
                 </div>
                 <!-- Reduced width of Jenis Kelamin field to col-sm-4 -->
                 <div class="col-sm-3">
                     <label>Jenis Kelamin</label><br>
                     <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required> Laki-laki
                     <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+                    <p class="text-danger" id="err_jenis_kelamin"></p>
                 </div>
             </div>
 
             <div class="form-group">
                 <label>Alamat</label>
                 <textarea name="alamat" id="alamat" class="form-control" required></textarea>
+                <p class="text-danger" id="err_alamat"></p>
             </div>
             <div class="form-group">
                 <label>No Telepon</label>
                 <input type="number" name="no_telp" id="no_telp" class="form-control" required>
+                <p class="text-danger" id="err_no_telp"></p>
             </div>
             <div class="form-group">
                 <button type="button" name="simpan" id="simpan" class="btn btn-primary">
@@ -78,6 +82,54 @@ include 'auth.php';
 
             $('.data').load('data.php');
         });
-    </script>
+
+        $("#simpan").click(function(){
+    var data = $('.form-data').serialize();
+    var jenkel1 = document.getElementById("jenkel1").value;
+    var jenkel2 = document.getElementById("jenkel2").value;
+    var nama = document.getElementById("nama").value;
+    var alamat = document.getElementById("alamat").value;
+    var no_telp = document.getElementById("no_telp").value;
+    
+    if (nama=="") {
+        document.getElementById("err_nama").innerHTML = "Nama Harus Diisi";
+    } else {
+        document.getElementById("err_nama").innerHTML = "";
+    }
+    
+    if (alamat=="") {
+        document.getElementById("err_alamat").innerHTML = "Alamat Harus Diisi";
+    } else {
+        document.getElementById("err_alamat").innerHTML = "";
+    }
+    
+    if (document.getElementById("jenkel1").checked==false && document.getElementById("jenkel2").checked==false ) {
+        document.getElementById("err_jenis_kelamin").innerHTML = "Jenis Kelamin Harus Dipilih";
+    } else {
+        document.getElementById("err_jenis_kelamin").innerHTML = "";
+    }
+    
+    if (no_telp=="") {
+        document.getElementById("err_no_telp").innerHTML = "No Telepon Harus Diisi";
+    } else {
+        document.getElementById("err_no_telp").innerHTML = "";
+    }
+    
+    if (nama!="" && alamat!="" && (document.getElementById("jenkel1").checked==true || document.getElementById("jenkel2").checked==true) && no_telp!="") {
+        $.ajax({
+            type: 'POST',
+            url: "form_action.php",
+            data: data,
+            success: function() {
+                $('.data').load("data.php");
+                document.getElementById("id").value = "";
+                document.getElementById("form-data").reset();
+            }, error: function(response) {
+                console.log(response.responseText);
+            }
+        });
+    }
+});
+</script>
 </body>
 </html>
