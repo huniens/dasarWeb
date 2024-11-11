1254d813eedbc08984e189239a4b2748cf72afb5
huniens <huniens06@gmail.com>
2024-11-11 19:38:47 +0700

    JOBSHEET 11 - PRACTICE 5

--- JS11_PHP/fungsi/hapus.php
+++ JS11_PHP/fungsi/hapus.php
@@ -6,6 +6,28 @@ if (!empty($_SESSION['username'])) {
     require '../fungsi/pesan_kilat.php';
     require '../fungsi/anti_injection.php';
 
+    // Handling deletion for anggota
+    if (!empty($_GET['anggota'])) {
+        $id = antiinjection($koneksi, $_GET['id']);
+        $query = "DELETE FROM user WHERE id = '$id'";
+
+        if (mysqli_query($koneksi, $query)) {
+            $query2 = "DELETE FROM anggota WHERE user_id = '$id'";
+
+            if (mysqli_query($koneksi, $query2)) {
+                pesan('success', "Anggota Telah Terhapus.");
+            } else {
+                pesan('warning', "Data Login Terhapus Tetapi Data Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+            }
+        } else {
+            pesan('danger', "Anggota Tidak Terhapus Karena: " . mysqli_error($koneksi));
+        }
+
+        header("Location: ../index.php?page=anggota");
+        exit;
+    }
+
+    // Handling deletion for jabatan
     if (!empty($_GET['jabatan'])) {
         $id = antiinjection($koneksi, $_GET['id']);
         $query = "DELETE FROM jabatan WHERE id = '$id'";
@@ -15,7 +37,11 @@ if (!empty($_SESSION['username'])) {
         } else {
             pesan('danger', "Jabatan Tidak Terhapus Karena: " . mysqli_error($koneksi));
         }
-    }
 
-    header("Location: ../index.php?page=jabatan");
+        header("Location: ../index.php?page=jabatan");
+        exit;
+    }
+} else {
+    header("Location: ../index.php"); // Redirect to login if not authenticated
+    exit;
 }
